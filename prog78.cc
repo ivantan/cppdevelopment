f03064bbfcc0a8d3b1528458549f44806937019c
ivantan <ivan.wjt@gmail.com>
2015-01-05 21:23:02 +0800

    3 jump statements; break, continue, goto.

---
+++ prog78.cc
@@ -0,0 +1,19 @@
+// Chap 4: Statements
+// 5.6 try blocks and exception handling
+
+#include <iostream>
+#include <vector>
+using std::vector;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::begin;
+using std::end;
+
+int main()
+{
+	// 
+		
+	return 0;
+}
