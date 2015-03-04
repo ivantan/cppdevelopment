7c573cc8258e3292c7f8b4829b469173762b2aff
ivantan <ivan.wjt@gmail.com>
2015-03-04 21:48:21 +0800

    examples of oloaded fnxs

---
+++ prog101.cc
@@ -0,0 +1,22 @@
+// Chap 6: Functions
+
+#include <iostream>
+#include <vector>
+#include <stdexcept>
+using std::vector;
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+using std::begin;
+using std::end;
+using std::initializer_list;
+
+// overloading and const parameters
+
+int main()
+{	
+	// 
+	
+	return 0;
+}
--- prog96.cc
+++ prog96.cc
@@ -19,12 +19,23 @@ using std::initializer_list;
 // but since a function can return a pointer, or a ref, to an
 // array, we will use it to alter the array.
 
+// the syntax however can be intimidating, so we use a method called
+// type alias
+
 int main()
 {
-	int ival = 5;
-	cout << factorial(ival) << endl;
-
-	for (int ival2 = 1; ival2 < 6; ival2++)
-			cout << factorial(ival2) << ", i = " << ival2 << endl;
+	typedef int arrT[10]; // arrT is a synonym for the type array of ten ints
+	using arrtT = int[10]; // equivalent decl of arrT
+	arrT* func(int i); // func returns a ptr to an array of five ints
+	
+	// without using a type alias
+	
+	int (*func(int i))[10];
+	
+	// recall
+	int arr[10]; // arr is an array of 10 ints
+	int *p1[10]; // p1 is an array of ten ptrs
+	int (*p2)[10] = &arr;  // p2 points to an array of ten ints
+	
 	return 0;
 }
