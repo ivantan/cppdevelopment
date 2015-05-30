cd891ee082c86b3df5bc384f71b5a7dbd3bd0ad1
Ivan Tan <ivan.wjt@gmail.com>
2015-05-31 00:11:39 +0800

    Use Sublime to compile and run prog10;
    executable was built on HP (windows) so it is .exe; prog11 is a WIP,
    with a range of operations to create character pictures.

--- acceleratedcpp/prog10.cc
+++ acceleratedcpp/prog10.cc
@@ -4,8 +4,11 @@
 using std::cin;         using std::cout;
 using std::endl;        using std::string;
 
+// to compare with the program in prog9.cc
+
 int main() {
     string s;
     while (cin >> s)
         cout << s << endl;
     return 0;
+}
