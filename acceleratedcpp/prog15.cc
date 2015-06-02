07adb878cf4eca704e0d4b27d54ad643503f86b5
ivantan <ivan.wjt@gmail.com>
2015-06-02 22:28:03 +0800

    prog15 is the abstract version of prog14; with functions in urls.cc and header url.h; compile each file into .o first

---
+++ acceleratedcpp/prog15.cc
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <cctype>
+#include <algorithm>
+
+#include "urls.h"
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::find_if;
+using std::getline;
+using std::string;
+using std::vector;
+
+int main() {
+        string s;
+        while (getline(cin, s)) {
+                vector<string> v = find_urls(s);
+                for (vector<string>::const_iterator i = v.begin();
+                        i != v.end(); ++i)
+                        cout << *i << endl;
+        }
+        return 0;
+}
