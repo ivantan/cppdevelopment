e1961e37e1a2674d6c2cf67ab1f06b8595e77364
ivantan <ivan.wjt@gmail.com>
2015-06-19 20:46:39 +0800

    arguments for main() function

--- acceleratedcpp/chapt10/intro_ptr.cc
+++ acceleratedcpp/chapt10/intro_ptr.cc
@@ -8,6 +8,16 @@ int main()
 	int* p = &x;
 	cout << x << endl;
 	
+	// here int* p is the same as int *p
+	
+	// but note that int* p, q is not equal to
+	// int *p, *q
+	
+	// to be more specific, int* p, q is equal to
+	// int *p
+	// int q
+	// or int (*p), q or
+	
 	*p = 6;
 	cout << x << endl;
 	
