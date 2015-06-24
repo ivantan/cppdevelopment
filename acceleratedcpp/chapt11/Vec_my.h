6bbf2b25031ae37f4a40ef2b5860ffc401770981
ivantan <ivan.wjt@gmail.com>
2015-06-24 22:04:12 +0800

    add constructors

--- acceleratedcpp/chapt11/Vec_my.h
+++ acceleratedcpp/chapt11/Vec_my.h
@@ -2,7 +2,10 @@
 
 template <class T> class Vec {
 public:
-    // interface 
+	// TODO utility function to use deal with private elements
+	Vec() { create(); }
+	explicit Vec(size_type n, const T& val = T()) { create(n, val); }
+	
 private:
   T* data;        // first element in the Vec
   T* limit;       // one past the last element in the Vec
