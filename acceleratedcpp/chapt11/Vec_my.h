b29160051b284542a5ac4cc54bb8ecf15a3b3197
ivantan <ivan.wjt@gmail.com>
2015-06-24 22:24:11 +0800

    added functions to return iterators

--- acceleratedcpp/chapt11/Vec_my.h
+++ acceleratedcpp/chapt11/Vec_my.h
@@ -11,11 +11,18 @@ public:
 	Vec() { create(); }
 	explicit Vec(size_type n, const T& val = T()) { create(n, val); }
 
-    // new operations: size and index
-    size_type size() const { return limit - data; }
+  // new operations: size and index
+  size_type size() const { return limit - data; }
 
-    T& operator[](size_type i) { return data[i]; }
-    const T& operator[](size_type i) const { return data[i]; }
+  T& operator[](size_type i) { return data[i]; }
+  const T& operator[](size_type i) const { return data[i]; }
+	
+  // new functions to return iterators
+  iterator begin() { return data; }                 // added
+  const_iterator begin() const { return data; }     // added
+
+  iterator end() { return limit; }                  // added
+  const_iterator end() const { return limit; }      // added
 private:
   iterator data;        // first element in the Vec
   iterator limit;       // one past the last element in the Vec
