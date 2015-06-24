dca79b4a24574d6c6552a70bc0c16aa9d3482923
ivantan <ivan.wjt@gmail.com>
2015-06-24 22:19:07 +0800

    change the implementation to allow for users of the template to use iterator and const_iterator

--- acceleratedcpp/chapt11/Vec_my.h
+++ acceleratedcpp/chapt11/Vec_my.h
@@ -3,10 +3,15 @@
 template <class T> class Vec {
 public:
 	// TODO utility function to use deal with private elements
+	typedef T* iterator;
+	typedef const T* const_iterator;
+	typedef size_t size_type;
+	type T value_type;
+	
 	Vec() { create(); }
 	explicit Vec(size_type n, const T& val = T()) { create(n, val); }
-	
+	// remaining interface
 private:
-  T* data;        // first element in the Vec
-  T* limit;       // one past the last element in the Vec
+  iterator data;        // first element in the Vec
+  iterator limit;       // one past the last element in the Vec
 };
