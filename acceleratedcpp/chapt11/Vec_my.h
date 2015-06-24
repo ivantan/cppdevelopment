55e7f3b463bbbb7e9e00ccf610a92a9bfc5f557a
ivantan <ivan.wjt@gmail.com>
2015-06-24 22:01:03 +0800

    use T to denote type, which will be set to the type during call; decalre data and limit, 2 pointers to denote array size

---
+++ acceleratedcpp/chapt11/Vec_my.h
@@ -0,0 +1,9 @@
+// implementing the Vec class
+
+template <class T> class Vec {
+public:
+    // interface 
+private:
+  T* data;        // first element in the Vec
+  T* limit;       // one past the last element in the Vec
+};
